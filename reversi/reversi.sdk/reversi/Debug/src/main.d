89bc0ad7caa231f657841ec419a1f128a1300025
DarFig <log.Dariel@gmail.com>
2018-03-22 21:20:28 +0100

    add sdk project

---
+++ reversi/reversi.sdk/reversi/Debug/src/main.d
@@ -0,0 +1,13 @@
+src/main.o src/main.o: ../src/main.c \
+ ../../reversi_bsp/ps7_cortexa9_0/include/matriz_celda.h \
+ ../../reversi_bsp/ps7_cortexa9_0/include/xil_types.h \
+ ../../reversi_bsp/ps7_cortexa9_0/include/xstatus.h \
+ ../../reversi_bsp/ps7_cortexa9_0/include/xil_assert.h
+
+../../reversi_bsp/ps7_cortexa9_0/include/matriz_celda.h:
+
+../../reversi_bsp/ps7_cortexa9_0/include/xil_types.h:
+
+../../reversi_bsp/ps7_cortexa9_0/include/xstatus.h:
+
+../../reversi_bsp/ps7_cortexa9_0/include/xil_assert.h:
---
+++ reversi/reversi.sdk/reversi/src/main.c
@@ -0,0 +1,9 @@
+/*
+ * main.c
+ *
+ *  Created on: 22 mar. 2018
+ *      Author: Dariel
+ */
+
+#include "matriz_celda.h"
+

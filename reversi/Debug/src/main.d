afc2143c3e40e385596e44240144534393354a12
DarFig <log.Dariel@gmail.com>
2018-04-05 21:55:02 +0200

    upd

---
+++ reversi/Debug/src/main.d
@@ -0,0 +1,45 @@
+src/main.o src/main.o: ../src/main.c \
+ ../../reversi_bsp/ps7_cortexa9_0/include/matriz_celda.h \
+ ../../reversi_bsp/ps7_cortexa9_0/include/xil_types.h \
+ ../../reversi_bsp/ps7_cortexa9_0/include/xstatus.h \
+ ../../reversi_bsp/ps7_cortexa9_0/include/xil_assert.h \
+ ../../reversi_bsp/ps7_cortexa9_0/include/xil_types.h \
+ ../../reversi_bsp/ps7_cortexa9_0/include/xparameters.h \
+ ../../reversi_bsp/ps7_cortexa9_0/include/xparameters_ps.h \
+ ../../reversi_bsp/ps7_cortexa9_0/include/xil_io.h \
+ ../../reversi_bsp/ps7_cortexa9_0/include/xil_printf.h \
+ ../../reversi_bsp/ps7_cortexa9_0/include/xparameters.h \
+ ../../reversi_bsp/ps7_cortexa9_0/include/xpseudo_asm.h \
+ ../../reversi_bsp/ps7_cortexa9_0/include/xreg_cortexa9.h \
+ ../../reversi_bsp/ps7_cortexa9_0/include/xpseudo_asm_gcc.h \
+ ../src/platform.h ../src/platform_config.h
+
+../../reversi_bsp/ps7_cortexa9_0/include/matriz_celda.h:
+
+../../reversi_bsp/ps7_cortexa9_0/include/xil_types.h:
+
+../../reversi_bsp/ps7_cortexa9_0/include/xstatus.h:
+
+../../reversi_bsp/ps7_cortexa9_0/include/xil_assert.h:
+
+../../reversi_bsp/ps7_cortexa9_0/include/xil_types.h:
+
+../../reversi_bsp/ps7_cortexa9_0/include/xparameters.h:
+
+../../reversi_bsp/ps7_cortexa9_0/include/xparameters_ps.h:
+
+../../reversi_bsp/ps7_cortexa9_0/include/xil_io.h:
+
+../../reversi_bsp/ps7_cortexa9_0/include/xil_printf.h:
+
+../../reversi_bsp/ps7_cortexa9_0/include/xparameters.h:
+
+../../reversi_bsp/ps7_cortexa9_0/include/xpseudo_asm.h:
+
+../../reversi_bsp/ps7_cortexa9_0/include/xreg_cortexa9.h:
+
+../../reversi_bsp/ps7_cortexa9_0/include/xpseudo_asm_gcc.h:
+
+../src/platform.h:
+
+../src/platform_config.h:
---
+++ reversi/Debug/src/platform.d
@@ -0,0 +1,16 @@
+src/platform.o src/platform.o: ../src/platform.c \
+ ../../reversi_bsp/ps7_cortexa9_0/include/xparameters.h \
+ ../../reversi_bsp/ps7_cortexa9_0/include/xparameters_ps.h \
+ ../../reversi_bsp/ps7_cortexa9_0/include/xil_cache.h \
+ ../../reversi_bsp/ps7_cortexa9_0/include/xil_types.h \
+ ../src/platform_config.h
+
+../../reversi_bsp/ps7_cortexa9_0/include/xparameters.h:
+
+../../reversi_bsp/ps7_cortexa9_0/include/xparameters_ps.h:
+
+../../reversi_bsp/ps7_cortexa9_0/include/xil_cache.h:
+
+../../reversi_bsp/ps7_cortexa9_0/include/xil_types.h:
+
+../src/platform_config.h:

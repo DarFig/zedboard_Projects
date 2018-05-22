b5adf0a01485481d113bd39bb4c5d422adbefe6a
DarFig <log.Dariel@gmail.com>
2018-05-22 17:29:34 +0200

    upd main.c
    - actualizada la escritura en registros
    - listo para la primera prueba en placa

--- generic binary/main.c
+++ generic binary/main.c
@@ -11,7 +11,7 @@
  *   ps7_uart    115200 (configured by bootrom/bsp)
  */
 
- 
+
  #include <stdio.h>
 #include "xil_types.h"
 #include <stdio.h>
@@ -19,36 +19,55 @@
 #include "xil_io.h"
 #include "platform.h"
 
-#include "unidad_general"
+#include "unidad_general.h"
+
+#define DIR_MAX 508
+#define DOSM 20
+
 
 int main()
 {
 	init_platform();
-	
+
 	u32 baseaddr = 0x43c00000;
-    u32 matriz[20];
-	for(int i = 0; i < 20; i++)
+	const u32 zero = 0x00000000;
+
+    u32 matriz[DOSM];
+	for(int i = 0; i < DOSM; i++)
 	{
-		matriz[i] = 0x00000000;
+		matriz[i] = zero;
 	}
 	matriz[7] = 0xd6; matriz[6]=0x51; matriz[5]= 0x2c; matriz[4]=0x96;
     matriz[3] = 0x5a; matriz[2]=0x0b; matriz[1]= 0xc9; matriz[0]=0x64;
-	
+
 
     u32 vector1 = 0x00;
 	u32 vector0 = 0x69;
     u32 salida1 = 0x0;
 	u32 salida0 = 0x0;
-	
+
 	print("....\n\r");
-    NEURONA_mWriteReg(baseaddr, , matriz[0]);
-    NEURONA_mWriteReg(baseaddr, , matriz[1]);
-    NEURONA_mWriteReg(baseaddr, , vector1);
-	NEURONA_mWriteReg(baseaddr, , vector0);
 
 
-    salida = NEURONA_mReadReg(baseaddr, XXXX_S00_AXI_SLV_REG1_OFFSET);
-    printf("resultado: %08x\n\r", salida);
+    int dir = 0;
+    for(int i = 0; i < DOSM; i++)
+	{
+    	UNIDAD_GENERAL_mWriteReg(baseaddr, UNIDAD_GENERAL_S00_AXI_SLV_REG0_OFFSET + dir, matriz[i]);
+		dir += 4;
+	}
+    while(DIR_MAX > dir)//--
+    {
+    	UNIDAD_GENERAL_mWriteReg(baseaddr, UNIDAD_GENERAL_S00_AXI_SLV_REG0_OFFSET + dir, zero);
+    	dir += 4;
+    }
+
+    UNIDAD_GENERAL_mWriteReg(baseaddr, UNIDAD_GENERAL_S00_AXI_SLV_REG129_OFFSET , vector1);
+    UNIDAD_GENERAL_mWriteReg(baseaddr, UNIDAD_GENERAL_S00_AXI_SLV_REG128_OFFSET, vector0);
+
+
+    salida1 = UNIDAD_GENERAL_mReadReg(baseaddr, UNIDAD_GENERAL_S00_AXI_SLV_REG131_OFFSET);
+    salida0 = UNIDAD_GENERAL_mReadReg(baseaddr, UNIDAD_GENERAL_S00_AXI_SLV_REG130_OFFSET);
+    printf("resultado: %08x %08x\n\r", salida1, salida0);
 
 
 	cleanup_platform();
